f478b559dbd8d8eddd1eec79b351fa922221c2fe
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2020-02-14 21:29:47 +0000

    spacemacs madness

---
+++ python/conway.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestConway(unittest.TestCase):
+	def test_should_return_empty_world_when_run_with_empty_world(self):
+        self.assertEquals()
+
+if __name__ == '__main__':
+	unittest.main()
+
