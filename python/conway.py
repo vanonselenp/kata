777912da75fb94df5d1b40759ee85d0b7032f9a2
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2020-02-28 09:38:23 +0000

    test of personal email

--- python/conway.py
+++ python/conway.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestConway(unittest.TestCase):
 	def test_should_return_empty_world_when_run_with_empty_world(self):
-        self.assertEquals()
+        self.assertEqual()
 
 if __name__ == '__main__':
 	unittest.main()
