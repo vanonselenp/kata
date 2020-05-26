24f67ceb0f512ed4b65501585283b39a89af8fb7
Peter Van Onselen <vanonselen.p@gmail.com>
2020-05-26 08:27:01 +0100

    play flair kata

--- python/playfliar.py
+++ python/playfliar.py
@@ -33,10 +33,10 @@
 #-    4 ) for each pair of letters: 
 #            we locate both letters in the cipher square. 
 #
-#There are four possible orientations for the pair of letters: 
+#There are four? possible orientations for the pair of letters: 
 #    - the "rectangle" case: they could be in different rows and columns, 
-#    - the "row" case: they could be in the same row but different columns (the "row" case), they could
-#be in the same column but different rows (the "column"
+#    - the "row" case: they could be in the same row but different columns (the "row" case),
+#    - the "col" they could be in the same column but different rows (the "column"
 #case), or they could be the same letter (the "same" case).
 #
 #Looking at the message PS HE LL OW OR LD SX:
@@ -103,17 +103,9 @@
 #
 # encrypt("PS. Hello, world") -> "QLGRQTVZIBTYQZ"
 # decrypt("QLGRQTVZIBTYQZ") -> "PSHELXOWORLDSX"
-#
-#HINT: You might find it easier if you implement some
-#helper functions, like a find_letter function that
-#returns the row and column of a letter in the cipher.
-#
-#HINT 2: Once you've written encrypt, decrypt should
-#be trivial: try to think of how you can modify encrypt
-#to serve as decrypt.
-#
-#To make this easier, we've gone ahead and created the
-#cipher as a 2D tuple for you:
+
+import unittest
+
 CIPHER = (("D", "A", "V", "I", "O"),
           ("Y", "N", "E", "R", "B"),
           ("C", "F", "G", "H", "K"),
@@ -121,16 +113,175 @@ CIPHER = (("D", "A", "V", "I", "O"),
           ("T", "U", "W", "X", "Z"))
 
 
+def format_characters(input):
+    uppered = "".join([x.upper() for x in input if x.isalpha()])
+    return uppered.replace("J", "I")
 
-#Add your code here!
+
+def pair_characters(input):
+    if (len(input) % 2 != 0):
+        input += "X"
+
+    result = []
+
+    for x in range(0, len(input), 2):
+        a = input[x]
+        second = input[x + 1]
+        b = second if second != a else "X"
+        result.append(a + b)
+
+    return result
 
 
+def prepare_data(input):
+    formatted = format_characters(input)
+    return pair_characters(formatted)
 
-#Below are some lines of code that will test your function.
-#You can change the value of the variable(s) to test your
-#function with different inputs.
-#
-#If your function works correctly, this will originally
-#print: QLGRQTVZIBTYQZ, then PSHELXOWORLDSX
-print(encrypt("PS. Hello, world"))
-print(decrypt("QLGRQTVZIBTYQZ"))
+
+def is_row_case(input):
+    for row in CIPHER:
+        if input[0] in row and input[1] in row:
+            return True
+    return False
+
+
+def apply_row_case(input, step=1):
+    def get_character(s):
+        return row[(row.index(s) + step) % 5]
+
+    for r in CIPHER:
+        if input[0] in r:
+            row = r
+            break
+
+    a = get_character(input[0])
+    b = get_character(input[1])
+    return "%s%s" % (a, b)
+
+
+def get_col(index):
+    return  [
+        CIPHER[0][index],
+        CIPHER[1][index],
+        CIPHER[2][index],
+        CIPHER[3][index],
+        CIPHER[4][index]
+    ]
+
+
+def is_col_case(input):
+    for i in range(0, 5):
+        col = get_col(i)
+        if input[0] in col and input[1] in col:
+            return True
+    return False
+
+
+def apply_col_case(input, step=1):
+    for index in range(0, 5):
+        col = get_col(index)
+        if input[0] in col:
+            break
+
+    a = col[(col.index(input[0]) + step) % 5]
+    b = col[(col.index(input[1]) + step) % 5]
+
+    return "%s%s" % (a, b)
+
+
+def apply_rectangle_case(input):
+    for first_row in CIPHER:
+        if input[0] in first_row:
+            break
+
+    for second_row in CIPHER:
+        if input[1] in second_row:
+            break
+
+    a = first_row[second_row.index(input[1])]
+    b = second_row[first_row.index(input[0])]
+
+    return "%s%s" % (a, b)
+
+
+def encrypt(input, step=1):
+    data = prepare_data(input)
+
+    result = []
+    for pair in data:
+        if is_row_case(pair):
+            result.append(apply_row_case(pair, step))
+        elif is_col_case(pair):
+            result.append(apply_col_case(pair, step))
+        else:
+            result.append(apply_rectangle_case(pair))
+
+    return "".join(result)
+
+
+def decrypt(input):
+    return encrypt(input, -1)
+
+
+class TestFliarEncrypt(unittest.TestCase):
+    def test_strip_non_alphabet_characters(self):
+        self.assertEqual(format_characters("a"), "A")
+
+    def test_replace_j_with_i(self):
+        self.assertEqual(format_characters("j"), "I")
+
+    def test_strip_white_space_from_characters(self):
+        self.assertEqual(format_characters("a b"), "AB")
+
+    def test_strip_numbers(self):
+        self.assertEqual(format_characters("a 1"), "A")
+
+    def test_return_in_pairs(self):
+        self.assertEqual(pair_characters("ABCD"), ["AB", "CD"])
+
+    def test_return_test_sentence_correctly(self):
+        expected = ["PS", "HE", "LX", "OW", "OR", "LD", "SX"]
+        self.assertEqual(prepare_data("PS. Hello, worlds"), expected)
+
+    def test_is_row_case(self):
+        self.assertEqual(is_row_case("PS"), True)
+
+    def test_is_not_row_case(self):
+        self.assertEqual(is_row_case("PH"), False)
+
+    def test_apply_row_case(self):
+        self.assertEqual(apply_row_case("PS"), "QL")
+
+    def test_is_col_case(self):
+        self.assertEqual(is_col_case("LD"), True)
+
+    def test_is_not_col_case(self):
+        self.assertEqual(is_col_case("DA"), False)
+
+    def test_apply_col_case(self):
+        self.assertEqual(apply_col_case("LD"), "TY")
+
+    def test_rectangle_case(self):
+        self.assertEqual(apply_rectangle_case("HE"), "GR")
+
+    def test_rectangle_case_2(self):
+        self.assertEqual(apply_rectangle_case("CS"), "KL")
+
+    def test_encrypt(self):
+        expected = "QLGRQTVZIBTYQZ"
+
+        actual = encrypt("PS. Hello, worlds")
+
+        self.assertEqual(actual, expected)
+
+    def test_decrypt(self):
+        expected = "PSHELXOWORLDSX"
+
+        actual = decrypt("QLGRQTVZIBTYQZ")
+
+        self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
